810dc35365a777bfa5a359317cb2be3ed3e255a3
rishikeshydv <rishikeshadh4@gmail.com>
2024-10-26 12:06:11 -0400

    feat: WritePump added

--- backend/websockets/client.go
+++ backend/websockets/client.go
@@ -53,7 +53,36 @@ func (c *Client) WritePump() {
 		c.conn.Close()
 	}()
 	for {
+		select {
+		case message, ok := <-c.send:
+			c.conn.SetWriteDeadline(time.Now().Add(writingWaitTime))
+			if !ok {
+				// The hub closed the channel.
+				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
 
+			w, err := c.conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			w.Write(message)
+
+			// Add queued chat messages to the current websocket message.
+			n := len(c.send)
+			for i := 0; i < n; i++ {
+				w.Write(<-c.send)
+			}
+
+			if err := w.Close(); err != nil {
+				return
+			}
+		case <-ticker.C:
+			c.conn.SetWriteDeadline(time.Now().Add(writingWaitTime))
+			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
 	}
 }
 func ServerWs(hub *Hub, w http.ResponseWriter, r *http.Request) {
